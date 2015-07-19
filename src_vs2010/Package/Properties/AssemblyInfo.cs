24f7bb7e8f568a2995d05804d97e2d91f46ea7d6
Pawel Hofman <pawel@codetitans.pl>
2015-07-20 00:25:16 +0200

    Bumped version

--- src_vs2010/Package/Properties/AssemblyInfo.cs
+++ src_vs2010/Package/Properties/AssemblyInfo.cs
@@ -18,7 +18,6 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using BlackBerry.NativeCore;
-using BlackBerry.Package;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
